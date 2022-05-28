0eee16cc27fcefae98746998de387164454d86f3
Yury <fiz12@bk.ru>
2022-05-28 19:51:59 +0300

    'test-1'

--- events/admin.py
+++ events/admin.py
@@ -3,11 +3,11 @@ from . import models
 
 
 @admin.register(models.Event)
-class EventdAdmin(admin.ModelAdmin):
+class EventAdmin(admin.ModelAdmin):
   pass
 
 
 @admin.register(models.Category)
-class CategorydAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
   pass
 # Register your models here.
